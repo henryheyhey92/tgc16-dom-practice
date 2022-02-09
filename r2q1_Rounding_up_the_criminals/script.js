f1a26872fff0ca24ee414741b1698055af928b88
Henry Lai <e0032134@u.nus.edu>
2022-02-09 10:01:51 +0000

    add q1 & q2

--- r2q1_Rounding_up_the_criminals/script.js
+++ r2q1_Rounding_up_the_criminals/script.js
@@ -1,3 +1,26 @@
 // PLACE YOUR CODE HERE
+// yarn add jest
+
+//1.
+//you are getting things into object, so need to loop through
+let arrested = document.querySelectorAll('.a');
+
+for(let s of arrested){
+    s.style.backgroundColor = "red";
+}
+
+//2.
+for(let y of arrested)
+{
+    y.innerText += " jailed"
+}
+
+let undercover = document.querySelectorAll('.a.undercover');
+
+for(let x of undercover)
+{
+    x.style.backgroundColor = "yellow";
+}
 
 
+//3.
--- r2q2_Dice_Throw/script.js
+++ r2q2_Dice_Throw/script.js
@@ -1,3 +1,20 @@
 // PLACE YOUR CODE HERE
+let btn = document.querySelector('#btn');
+let left = document.querySelector('#left');
+let right = document.querySelector('#right');
+let res = document.querySelector('#result');
+
+btn.addEventListener('click', function(){
+    let rndIntLeft = Math.floor(Math.random() * 6) + 1;
+    let rndIntRight = Math.floor(Math.random() * 6) + 1;
+
+    left.innerText = rndIntLeft;
+    right.innerText = rndIntRight;
+    let end = parseInt(rndIntLeft)+parseInt(rndIntRight);
+    res.innerText = `result is : ${end}`;
+})
+
+
+
 
 
