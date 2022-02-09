f1b3c29fb9f20d0f8a2ddd95c7aa399b4c2f250d
Henry Lai <e0032134@u.nus.edu>
2022-02-09 09:21:17 +0000

    add Q9 solution

--- q9_basic_form_manipulation/script.js
+++ q9_basic_form_manipulation/script.js
@@ -3,11 +3,31 @@ function getFormValues() {
 
     let email = document.querySelector('#email-address').value;
 
-    let model = document.querySelectorAll('.phone-type');
+    //for radio button no need to store 
+    let m = document.querySelectorAll('.phone-type');
+    for (let p of m) {
+        // for each rating radio button, check if its .checked
+        // is true
+        if (p.checked == true) {
+            model = p.value;
+            break;
+        }
+    }
 
-    let services = document.querySelectorAll('.services');
+    //use array because it will store multiple values for checkboxes
+    let services  = []; 
+    let s = document.querySelectorAll('.services');
+    for(let sc of s)
+    {
+        if(sc.checked == true)
+        {
+            services.push(sc.value);
+        }
+    }
 
-    let pickup = document.querySelectorAll('#pick-up');
+    //    let pickup = document.querySelector('#pick-up>option:checked').value;
+
+    let pickup = document.querySelector('#pick-up').value;
 
     // leave this line alone!
     return [email, model, services, pickup];
