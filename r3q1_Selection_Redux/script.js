8a5449937ce2c889bdc9ba4582bbbc9f0e91a325
Henry Lai <e0032134@u.nus.edu>
2022-02-13 13:21:46 +0000

    add R3 Q1

--- r3q1_Selection_Redux/script.js
+++ r3q1_Selection_Redux/script.js
@@ -1,3 +1,31 @@
 // PLACE YOUR CODE HERE
 
 
+//1.
+let h2 = document.querySelector('h2');
+
+h2.style.fontFamily = "Verdana";
+h2.style.fontSize = "32px";
+
+//2.
+let img = document.querySelector('img');
+img.style.border = "1px solid black";
+img.style.paddingTop = "32px";
+img.style.paddingBottom = "15px";
+
+//3.
+let facts = document.querySelector('li:first-child');
+let facts2 = document.querySelector('li:last-child');
+
+facts.style.backgroundColor = "yellow";
+facts2.style.backgroundColor = "yellow";
+
+//4.
+let pan = document.querySelector('h2#pancake');
+pan.innerHTML = "Pancake Recipe";
+
+let all = document.querySelectorAll('ol>li');
+
+for(let a of all){
+    a.style.backgroundColor = "orange";
+}
