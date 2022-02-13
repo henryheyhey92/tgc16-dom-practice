1ef05dfcf02c894f343aad6aaaceab4b21a9d01e
Henry Lai <e0032134@u.nus.edu>
2022-02-13 14:26:57 +0000

    add R3 Q2 files

--- r3q2_Change_on_event/index.js
+++ r3q2_Change_on_event/index.js
@@ -1,3 +1,9 @@
 let box = document.getElementById('the-box');
 
-box.addEventListener(...)
+box.addEventListener('mouseenter', function(){
+    box.style.backgroundColor = "yellow";
+})
+
+box.addEventListener('mouseleave', function(){
+    box.style.backgroundColor = "white";
+})
