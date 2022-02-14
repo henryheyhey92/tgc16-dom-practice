591f9960d1bda1264af5eac602b573ceb6b69a86
Henry Lai <e0032134@u.nus.edu>
2022-02-14 15:34:15 +0000

    changes in R3 Q4

--- r3q4_Conditional_Forms/script.js
+++ r3q4_Conditional_Forms/script.js
@@ -6,21 +6,4 @@ let lastName = document.querySelector('#lastName');
 
 let option = document.querySelectorAll('.option');
 
-let forLocal = document.querySelector('#for-locals');
-
-let forTourist = document.querySelector('#for-tourists');
-
-document.body.addEventListener('change', function(){
-    switch(option.value)
-    {
-        case 'local':
-              forLocal.style.visibility = "visible";
-              forTourist.style.display = "none";
-              break;
-        case 'tourist':
-             forLocal.style.display = "none";
-             forTourist.style.visibility = "visible";    
-             break;
-
-    }
-})
+document.querySelector('#for-tourists').style.display = "none";
