cce6a130f2a51c91c0d53775760200065fe1dc2f
Henry Lai <e0032134@u.nus.edu>
2022-02-14 13:44:11 +0000

    add R3 Q3

--- r3q3_Change_on_Event_part_2/script.js
+++ r3q3_Change_on_Event_part_2/script.js
@@ -1,3 +1,18 @@
 // PLACE YOUR CODE HERE
 
+let box = document.querySelectorAll('.box');
+let len = box.length;
 
+for(let i of box){
+    i.addEventListener('mouseenter', function(){
+
+        i.style.backgroundColor = "yellow";
+       
+    })
+}
+
+for(let i of box){
+    i.addEventListener('mouseleave', function(){
+        i.style.backgroundColor = "white";
+    })
+}
--- r3q4_Conditional_Forms/script.js
+++ r3q4_Conditional_Forms/script.js
@@ -1,3 +1,26 @@
 // PLACE YOUR CODE HERE
 
+let firstName = document.querySelector('#firstName');
 
+let lastName = document.querySelector('#lastName');
+
+let option = document.querySelectorAll('.option');
+
+let forLocal = document.querySelector('#for-locals');
+
+let forTourist = document.querySelector('#for-tourists');
+
+document.body.addEventListener('change', function(){
+    switch(option.value)
+    {
+        case 'local':
+              forLocal.style.visibility = "visible";
+              forTourist.style.display = "none";
+              break;
+        case 'tourist':
+             forLocal.style.display = "none";
+             forTourist.style.visibility = "visible";    
+             break;
+
+    }
+})
